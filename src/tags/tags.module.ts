ad3c267bf3fdf787302668d8dca0d52931b7680b
lelderbe <fvdc@ya.ru>
2022-01-15 13:26:58 +0300

    added Tag entity

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TagsController } from '@/tags/tags.controller';
 import { TagsService } from '@/tags/tags.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tag } from './entities/tag.entity';
 
 @Module({
+	imports: [TypeOrmModule.forFeature([Tag])],
 	controllers: [TagsController],
 	providers: [TagsService],
 })
