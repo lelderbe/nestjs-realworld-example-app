33c5ff2f2fa14451b32f82a6f7c17ff0baadcb08
lelderbe <fvdc@ya.ru>
2022-01-16 19:11:43 +0300

    GET /user realized

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserInput } from '@/users/dto/create-user.input';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { CurrentUser } from '@/users/decorators/user.decorator';
 import { IAuthResponse } from './types/auth-response.interface';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller()
 export class AuthController {
@@ -22,6 +23,12 @@ export class AuthController {
 	@Post('users/login')
 	async login(@CurrentUser() user): Promise<IAuthResponse> {
 		// console.log('AuthController login()');
+		return this.authService.login(user);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get('user')
+	async findOne(@CurrentUser() user): Promise<IAuthResponse> {
 		return this.authService.login(user);
 	}
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
-import { User } from './entities/user.entity';
-import { UsersService } from './users.service';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class UsersController {
-	constructor(private readonly usersService: UsersService) {}
-
-	// @Post('users')
-	// async createUser(@Body('user') input: CreateUserInput): Promise<User> {
-	// 	console.log('input', input);
-	// 	return this.usersService.create(input);
-	// }
-}
+export class UsersController {}
